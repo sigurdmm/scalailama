a3b1b602219d9618141a33d2de1cd7959cd6a173
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2019-10-18 11:40:57 +0200

    Rewrite account operations to use Either

--- src/main/scala/Account.scala
+++ src/main/scala/Account.scala
@@ -9,17 +9,32 @@ class Account(val bank: Bank, initialBalance: Double) {
     // TODO
     // for project task 1.2: implement functions
     // for project task 1.3: change return type and update function bodies
-    def withdraw(amount: Double) {
+    def withdraw(amount: Double): Either[Unit, Exception] = {
         // See Synchronization, https://twitter.github.io/scala_school/concurrency.html
         balance.synchronized {
-            balance.amount -= amount
+            val nextBalance = balance.amount - amount
+
+            if (amount <= 0) {
+                Right(new IllegalAmountException("You cannot withdraw a negative amount"))
+            } else if (nextBalance < 0) {
+                Right(new NoSufficientFundsException(s"You are missing ${nextBalance * -1}"))
+            } else {
+                balance.amount = nextBalance
+                Left(Unit)
+            }
         }
     }
-    def deposit (amount: Double) {
+    def deposit (amount: Double): Either[Unit, Exception] = {
         balance.synchronized {
+            if (amount <= 0) {
+                return Right(new IllegalAmountException("You cannot deposit a negative amount"))
+            }
+
             balance.amount += amount
+            Left(Unit)
         }
     }
+
     def getBalanceAmount: Double = balance.amount
 
     def transferTo(account: Account, amount: Double) {
